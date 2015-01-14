15fe06bf0b0a29de9cfcfeeb76c742108a7b7b00
Alexander Auriel <alexandermagus6@icloud.com>
2015-01-14 07:51:03 -0500

    files cleanup

--- Users/_vti_cnf/bookmarks.sql
+++
@@ -1,6 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|06 Jul 2004 02:41:06 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_cacheddtm:TX|06 Jul 2004 02:41:06 -0000
-vti_filesize:IR|391
-vti_backlinkinfo:VX|
--- js/state/_vti_cnf/Repair.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|29 Dec 2014 07:27:53 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|AutoObsessionsGame/index.html
-vti_author:SR|ALEXS\\TzL
-vti_modifiedby:SR|ALEXS\\TzL
-vti_timecreated:TR|18 Dec 2014 18:35:22 -0000
-vti_cacheddtm:TX|29 Dec 2014 07:27:53 -0000
-vti_filesize:IR|1863
