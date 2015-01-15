35604bc6a698ac746e9c49e2bbc3d5a544e211d0
Alexander Auriel <alexandermagus6@icloud.com>
2015-01-15 11:45:08 -0500

    dbconnect changes almost connecting

--- Users/db_fns.php
+++ Users/db_fns.php
@@ -1,14 +1,16 @@
 <?php
 
-function db_connect() {
-	
-   $result = new mysqli('localhost', 'root', 'Dante777', 'members');
-   if (!$result) 
-   {
+function db_connect() 
+{
+   $host= 'localhost';
+   $db = 'bookmarks';
+   $manager = 'root';
+   $passW = 'Dante777';
+   $port = '14147';
+   $result = new mysqli($host, $manager , $passW, $db,$port);
+   if (!$result) {
      throw new Exception('Could not connect to database server');
-   } 
-   else 
-   {
+   } else {
      return $result;
    }
 }
