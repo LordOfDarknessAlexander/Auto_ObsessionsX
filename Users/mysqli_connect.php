cd8eceb2e523b4de7df3bdeaf2818868042d506d
Alexander Auriel <alexandermagus6@icloud.com>
2015-01-20 12:02:08 -0500

    merge updates db connect class

--- Users/mysqli_connect.php
+++ Users/mysqli_connect.php
@@ -4,39 +4,29 @@
 // Set the access details as constants:
 //TODO:implement reusable sql connection interface
 //
-//class dbConnect
-//{ //class constants don't need to be prefixed with '$'!
-    //private $_user,
-        //$_pw,
-        //$_host,
-        //$_name;
+class dbConnect
+{ 
+	//class constants don't need to be prefixed with '$'!
+    private $_user,
+        $_pw,
+        $_host,
+        $_name;
         
-    //public $con;   //database connection;
-    //public function __construct($user = 'root', $password = 'Dante777', $host = 'localhost', $dbName = 'finalPost')
-    //{ //unsuccessful creation of this object suppresses any exception thrown and kills the executing program
-        //self::$con = @mysqli_connect(HOST, USER,PW, NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
-        //mysqli_set_charset(self::$con, 'utf8');
-    //}
-    ////public function __destruct(){
-        //disconnect from database when this is 'unset()'
-        //mysqli_close(self::$con);
-    //}
-    //public function strip($postKey){
-        //strip HTML and apply escaping from var passed to page as POST arg
-        //$res = trim($_POST[$postKey]);
-        //return mysqli_real_escape_string($self::$con, strip_tags($res));
-    //}
+    public $con;   //database connection;
+    public function __construct($user = 'root', $password = 'Dante777', $host = 'localhost', $dbName = 'finalPost')
+    { //unsuccessful creation of this object suppresses any exception thrown and kills the executing program
+        self::$con = @mysqli_connect(HOST, USER,PW, NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+        mysqli_set_charset(self::$con, 'utf8');
     }
-//}
-//$db = dbConnect();
-//
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PASSWORD', 'Dante777');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'finalpost');
-
-// Make the connection:
-$dbcon = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
-
-// Set the encoding...
-mysqli_set_charset($dbcon, 'utf8');
+    public function __destruct()
+	{
+        //disconnect from database when this is 'unset()'
+        mysqli_close(self::$con);
+    }
+    public function strip($postKey)
+	{
+        //strip HTML and apply escaping from var passed to page as POST arg
+        $res = trim($_POST[$postKey]);
+        return mysqli_real_escape_string($self::$con, strip_tags($res));
+   }
+}
--- phtml/register.php
+++ phtml/register.php
@@ -2,26 +2,43 @@
 <?php
  //require_once('AOUsers_include.php');
 ?>	
-    <form method="post" action="Users/register_new.php">
-	    <table bgcolor="#cccccc">
-	        <tr>
-	        <td>Email address:</td>
-	        <td><input type="text" name="email" size="30" maxlength="100"/></td></tr>
-	    <tr>
-	        <td>Preferred username <br />(max 16 chars):</td>
-	        <td valign="top"><input type="text" name="username" size="16" maxlength="16"/></td></tr>
-	    <tr>
-	        <td>Password <br />(between 6 and 16 chars):</td>
-	        <td valign="top"><input type="password" name="passwd" size="16" maxlength="16"/></td></tr>
-	    <tr>
-	        <td>Confirm password:</td>
-	        <td><input type="password" name="passwd2" size="16" maxlength="16"/></td></tr>
-	    <tr>
-	        <td colspan=2 align="center">
-	        <input type="submit" value="Register"></td>
-            </tr>
-	    </table>
-    </form>	
+    <h2>Membership Registration</h2>
+		<h3 class="content">Items marked with an asterisk * are essential</h3>
+			<h3 class="content">When you click the 'Register' button, you will 
+			be switched to a page<br>for paying your membership fee with PayPal or a Credit/Debit 
+			card</h3>
+			<p class="cntr"><b>Membership classes:</b> Standard 1 year: &pound;30, Standard 5years: 
+			&pound;125, Armed Forces 1 year: &pound;5,<br>Under 21 one year: &pound;2,&nbsp; Other: If 
+			you can't afford &pound;30 please give what you can, minimum &pound;15 </p>
+			<fieldset>
+	<form action="Users/safer-register-page.php" method="post">
+		<label class="label" for="title">Title*</label><input id="title" type="text" name="title" size="15" maxlength="12" value="<?php if (isset($_POST['title'])) echo $_POST['title']; ?>">
+		<br><label class="label" for="fname">First Name*</label><input id="fname" type="text" name="fname" size="30" maxlength="30" value="<?php if (isset($_POST['fname'])) echo $_POST['fname']; ?>">
+		<br><label class="label" for="lname">Last Name*</label><input id="lname" type="text" name="lname" size="30" maxlength="40" value="<?php if (isset($_POST['lname'])) echo $_POST['lname']; ?>">
+		<br><label class="label" for="email">Email Address*</label><input id="email" type="text" name="email" size="30" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" >
+		<br><label class="label" for="psword1">Password*</label><input id="psword1" type="password" name="psword1" size="12" maxlength="12" value="<?php if (isset($_POST['psword1'])) echo $_POST['psword1']; ?>" >&nbsp;8 
+		to 12 characters
+		<br><label class="label" for="psword2">Confirm Password*</label><input id="psword2" type="password" name="psword2" size="12" maxlength="12" value="<?php if (isset($_POST['psword2'])) echo $_POST['psword2']; ?>" >
+		<br><label class="label" for="uname">Secret User Name*</label><input id="uname" type="text" name="uname" size="12" maxlength="12" value="<?php if (isset($_POST['uname'])) echo $_POST['uname']; ?>">&nbsp;6 
+		to 12 characters
+		<br><label class="label" for="class">Membership Class*</label>
+		<select name="class">
+		<option value="">- Select -</option>
+		<option value="30"<?php if (isset($_POST['class']) AND ($_POST['class'] == '30')) echo ' selected="selected"'; ?>>Standard 1 year &pound;30</option>
+		<option value="125"<?php if (isset($_POST['class']) AND ($_POST['class'] == '125')) echo ' selected="selected"'; ?>>Standard 5 years &pound;125</option>
+		<option value="5"<?php if (isset($_POST['class']) AND ($_POST['class'] == '5')) echo ' selected="selected"'; ?>>Armed Forces 1 year &pound;5</option>
+		<option value="2"<?php if (isset($_POST['class']) AND ($_POST['class'] == '2')) echo ' selected="selected"'; ?>>Under 22 1 year &pound;2**</option>
+		<option value="15"<?php if (isset($_POST['class']) AND ($_POST['class'] == '15')) echo ' selected="selected"'; ?>>Minimum 1 year &pound;15</option>
+		</select>
+		<br><label class="label" for="addr1">Address*</label><input id="addr1" type="text" name="addr1" size="30" maxlength="30" value="<?php if (isset($_POST['addr1'])) echo $_POST['addr1']; ?>">
+		<br><label class="label" for="addr2">Address</label><input id="addr2" type="text" name="addr2" size="30" maxlength="30" value="<?php if (isset($_POST['addr2'])) echo $_POST['addr2']; ?>">
+		<br><label class="label" for="city">City*</label><input id="city" type="text" name="city" size="30" maxlength="30" value="<?php if (isset($_POST['city'])) echo $_POST['city']; ?>">
+		<br><label class="label" for="county">County*</label><input id="county" type="text" name="county" size="30" maxlength="30" value="<?php if (isset($_POST['county'])) echo $_POST['county']; ?>">
+		<br><label class="label" for="pcode">Post Code*</label><input id="pcode" type="text" name="pcode" size="15" maxlength="15" value="<?php if (isset($_POST['pcode'])) echo $_POST['pcode']; ?>">
+		<br><label class="label" for="phone">Telephone</label><input id="phone" type="text" name="phone" size="30" maxlength="30" value="<?php if (isset($_POST['phone'])) echo $_POST['phone']; ?>">
+		<p><input id="submit" type="submit" name="submit" value="Register"></p>
+	</form>
+</fieldset>
 </div>
 <!---
 <div id='fg_membersite_content'>
