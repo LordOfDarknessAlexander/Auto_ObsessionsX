7869ffbff80c5c84e3ce2231dc5cf0c4a9524b70
Alexander Auriel <alexandermagus6@icloud.com>
2015-01-26 22:15:36 -0500

    Users cleanup code ph 1

--- Users/includes/nav.php
+++ Users/includes/nav.php
@@ -2,8 +2,8 @@
 <ul>
 <li><a href="game.php" title="Play Game">Play as Guest</a></li>
 <li><a href="tutorial.php" title="Tutorial">Tutorial</a></li>
-<li><a href="page-4.php" title="Credits">Credits</a></li>
-<li><a href="page-5.php" title="Player Profile">Profile</a></li>
+<li><a href="credits.php" title="Credits">Credits</a></li>
+<li><a href="profiles.php" title="Player Profile">Profile</a></li>
 <li><a href="index.php" title="Return to Home Page">Home Page</a></li>
 </ul>
 </div><!--end of side column and menu -->
--- Users/login.php
+++ Users/login.php
@@ -75,7 +75,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 ?>
 <!-- Display the form fields-->
 <div id="loginfields">
-<?php include ('includes/login_page.inc.php'); ?>
+<h2>Login</h2>
+<form action="login.php" method="post">
+	<p><label class="label" for="email">Email Address:</label>
+	<input id="email" type="text" name="email" size="30" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" > </p>
+		<p><label class="label" for="psword">Password:</label>
+	<input id="psword" type="password" name="psword" size="12" maxlength="12" value="<?php if (isset($_POST['psword'])) echo $_POST['psword']; ?>" ><span>&nbsp;Between 8 and 12 characters.</span></p>
+	<p><input id="submit" type="submit" name="submit" value="Login"></p>
+</form><br>
 </div>
 <br>
 <?php include ('includes/footer.php'); ?>
