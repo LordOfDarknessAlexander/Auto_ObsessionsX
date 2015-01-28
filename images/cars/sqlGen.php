d9bcd7e0287dab90cdf4a44f46b2fb2ecbf85552
Tyler Drury <that_canadianguy@hotmail.com>
2015-01-28 14:41:15 -0500

    aoCars DB PHP generator
    uses php to dynamically update table entries base on the structure of
    ./images/cars/

---
+++ images/cars/sqlGen.php
@@ -0,0 +1,121 @@
+<?php
+//This script generates entries into the vehicle sql database table aoCars!
+require_once '../../include/dbConnect.php';
+require_once '../../include/secure.php';
+//secure::adminLogin();
+echo 'executing script in directory: ' . __DIR__;?><br>
+<?php
+function getMakeHash($str){
+    //0 is a reserved value and is not used
+    if($str == 'Audi'){
+        return 0x01000000;
+    }elseif($str == 'Bentley'){
+        return 0x02000000;
+    }elseif($str == 'BMW'){
+        return 0x03000000;
+    }elseif($str == 'Buick'){
+        return 0x04000000;
+    }elseif($str == 'Chevrolet'){
+        return 0x05000000;
+    }elseif($str == 'Ferrari'){
+        return 0x06000000;
+    }elseif($str == 'Ford'){
+        return 0x07000000;
+    }elseif($str == 'Jaguar'){
+        return 0x08000000;
+    }elseif($str == 'GMC'){
+        return 0x09000000;
+    }
+?><br><?php
+    echo 'invalid value: ' . $str;
+?><br><?php
+    //exit(); //kill the script if wrong value, don't want 
+    /*
+07-
+08-
+09-Lamborghini
+0A-Porsche
+0B-Shelby
+0C-
+0D-
+0E-
+0F-
+//
+10-
+11-
+12-*/
+}
+foreach(new DirectoryIterator(__DIR__) as $file){
+    $id = 0;
+    //foreach manufacturer
+    if($file->isDir() && !$file->isDot() ){
+        $path = $file->getPathname();   //full path and file name
+        $make = $file->getBasename();   //name of trailing dir
+        $mHash = getMakeHash($make);
+        //$next = __DIR__ . '\\' . $path;
+        echo $path;?><br>
+        <?php
+        foreach(new DirectoryIterator($path) as $years){
+            //for each year folder
+            if($years->isDir() && !$years->isDot()){
+                $p = $years->getPathname();
+                $year = $years->getBasename();
+                //shift integer so padding for make and model
+                $yHash = (intval($year) - 1908) << 8;    //0x0000FFFF << 8;
+                //$next = __DIR__  . '\\' . $p;
+                echo $p;?><br>
+                <?php
+                $nameHash = 0x00000000;
+                foreach(new DirectoryIterator($p) as $name){
+                    //for each car in folder
+                    if($name->isFile() ){
+                        if($name->getExtension() == 'jpg'){
+                            //cap the number of cars to 255(0xFF) per year
+                            if($nameHash < 0xFF){
+                                $nameHash++;
+                            }
+                            $n = $name->getBasename('.jpg');
+                            $model = $n;
+                            echo '    ' . $n;?><br>
+                            <?php
+                            if(isset($make) && isset($year) && isset($model) ){
+                                $id = $mHash | $yHash | $nameHash;
+                                $y = intval($year);
+                                echo 'variables set!';
+                                $q = "INSERT INTO aoCars 
+                                (`car_id`, `make`, `year`, `model`, `price`, `info`) 
+                                VALUE
+                                ($id, '$make', $y, '$n', 5000, 'default info');";
+                                // ON DUPLICATE KEY UPDATE VALUES();";
+?><br><?php
+echo $q;
+?><br><?php
+                                if($AO_DB->query($q) == TRUE){
+                                    echo 'add element worked!';
+                                }else{
+                                    echo 'failed to add entry, error: ' . mysqli_error($AO_DB->con);
+                                    ?><br><?php
+                                }                           
+                            }
+                        }
+                    }
+                    else{
+                        //skipping directory or file
+                    }
+                }
+            }
+        }
+    }
+    if($file->isFile() ){
+        //do nothing
+    }
+}
+
+//$q = "INSERT INTO aoCars (`car_id`, `make`, `year`, `model`, `price`, `info`) VALUE ($id, '$m', $y, '$n', 5000, 'default info');"// ON DUPLICATE KEY UPDATE VALUES();";
+
+//if($AO_DB->query($q) == TRUE){
+    //echo 'add element worked!';
+//}else{
+    //echo 'Failed to add element, error occurred: ' . mysqli_error($AO_DB->con);
+//}
+?>
--- include/secure.php
+++ include/secure.php
@@ -1,24 +1,28 @@
 <?php
 //session_start();  //the inclusion of this page starts the session if not started, else resume current session
-function secureMemberLogin(){
-    //if not already logged in as a registered user, display login page
-    if(!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1) )
-    {
-        header("Location: login.php");  //redirect to user login.php
-        exit(); //exit the executing script, not the function, after dispalying secure login page
+class secure
+{   //static api for the secure login of user/admins/devs/etc, across pages
+    public static function memberLogin(){
+        //if not already logged in as a registered user, display login page
+        if(!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1) )
+        {
+            header("Location: login.php");  //redirect to user login.php
+            exit(); //exit the executing script, not the function, after dispalying secure login page
+        }
+        //else, display the page contents following calls to this function
     }
-    //else, display the page contents following calls to this function
-}
-function secureAdminLogin(){
-    //if not logged in as an admin, display login page
-    $accessDenied = 0;
-    $admin = 1;
-    $accessLevel = isset($_SESSION['user_level']) ? $_SESSION['user_level'] : $accessDenied;
-    
-    if($accessLevel != $admin) ){
-        //header("Location: adminLogin.php");  //redirect to user login.php
-        exit(); //exit the executing script, not the function, after dispalying secure login page
+    public static function adminLogin(){
+        //if not logged in as an admin, display login page
+        $accessDenied = -1;
+        $admin = 1;
+        $accessLevel = isset($_SESSION['user_level']) ? $_SESSION['user_level'] : $accessDenied;
+        
+        if($accessLevel != $admin){
+            echo 'You are not logged in and do not have access to this page!';
+            //header("Location: adminLogin.php");  //redirect to user login.php
+            exit(); //exit the executing script, not the function, after dispalying secure login page
+        }
+        //else, display the page contents following calls to this function
     }
-    //else, display the page contents following calls to this function
 }
 ?>
