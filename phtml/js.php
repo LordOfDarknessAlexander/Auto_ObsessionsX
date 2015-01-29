1cfb925f2312b461f63fa9751b90fb75a1eb46f9
Tyler Drury <that_canadianguy@hotmail.com>
2015-01-29 14:16:14 -0500

    entities folder
    moved player, vehicle, and enemy js into an entities folder for all game
    objects

--- phtml/js.php
+++ phtml/js.php
@@ -17,14 +17,19 @@ $paths = array(
     '2D/Animation',
     '2D/Sprite',
     //game entities
-    'Player',
-    'Enemy',
-    'vehicle',
+    'entities/player',
+    'entities/enemy',
+    'entities/vehicle',
     //game states
     'state/Garage',
+    //'state/Garage/main',
+    //'state/Garage/carView',
     //Auction and Repair states is dependant upon userGarage in Garage.js
     //to push cars or modify cars in the users garage, so parse first
     'state/Repair',
+    //'state/Auction/main',
+    //'state/Auction/select',
+    //'state/Auction/sell',
     'state/Auction',
     'state/AuctionSelect',
     'state/AuctionSell',
