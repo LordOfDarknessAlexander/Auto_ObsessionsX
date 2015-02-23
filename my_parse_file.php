a999628569043b4ccf3f072091fb12c6430c27a7
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-02-23 04:13:33 -0500

    update

--- my_parse_file.php
+++ my_parse_file.php
@@ -1,58 +1,39 @@
 <?php 
 require ('Users/config.php');
 
-if( isset( $_POST) )
-{
-	if ( !empty($_POST['money']) || !empty($_POST['tokens']) || !empty($_POST['prestige']) || !empty($_POST['m_marker']) || !empty($_POST['uname']))
-	{
-			
-			$money = $_POST["money"];
-			$tokens = $_POST["tokens"];
-			$prestige = $_POST["prestige"];
-			$marker = $_POST["m_marker"];
-			$uname = $_POST["uname"];
+session_start();
 
-			$q = "UPDATE users SET money='$money', tokens='$tokens' ,prestige='$prestige', m_marker='$marker', uname='$uname' WHERE  user_id = 1";
-			echo "Shits awesome";
-			$result = mysqli_query ($dbcon, $q);
-			if(!$result )
-			{
-			  die('Could not update data: ' . mysql_error());
-			}
-			else
-			{
-			  echo '{"money":"' . $money . '", "tokens":"' . $tokens . '","prestige":"' . $prestige . ',"m_marker":"' . $marker . ',"uname":"' . $uname . '"}';
-		
-			}
-			mysqli_close($dbcon);	
-			
-	}/*
-	else if( empty($_POST['money']) || empty($_POST['tokens']) || empty($_POST['prestige']) || empty($_POST['m_marker']) || !empty($_POST['uname']))
-	{
-		$money = $_POST["money"];
-		$tokens = $_POST["tokens"];
-		$prestige = $_POST["prestige"];
-		$marker = $_POST["m_marker"];
-		
-		$q = "UPDATE users SET money='$money', tokens='$tokens' ,prestige='$prestige', m_marker='$marker' WHERE user_id=2";
-		echo "Shits awesome";
-		$result = mysqli_query ($dbcon, $q);
-		if(!$result )
-		{
-		  die('Could not update data: ' . mysql_error());
-		}
-		else
-		{
-		  echo '{"money":"' . $money . '", "tokens":"' . $tokens . '","prestige":"' . $prestige . ',"m_marker":"' . $marker . '"}';
-	
-		}
-		mysqli_close($dbcon);	
-	}*/
-
-}
-else
+if (isset($_SESSION['uname']))
 {
-	echo "Goody try";
+	echo "{$_SESSION['uname']}";
+	if( isset( $_POST) )
+	{
+		if ( !empty($_POST['money']) || !empty($_POST['tokens']) || !empty($_POST['prestige']) || !empty($_POST['m_marker']))
+		{
+				$money = $_POST["money"];
+				$tokens = $_POST["tokens"];
+				$prestige = $_POST["prestige"];
+				$marker = $_POST["m_marker"];
+				$q = "UPDATE users SET money='$money', tokens='$tokens' ,prestige='$prestige', m_marker='$marker' WHERE   uname = '$_SESSION[uname]'";
+				echo "Awesome";
+				$result = mysqli_query ($dbcon, $q);
+				if(!$result )
+				{
+				  die('Could not update data: ' . mysql_error());
+				}
+				else
+				{
+				  echo '{"money":"' . $money . '", "tokens":"' . $tokens . '","prestige":"' . $prestige . ',"m_marker":"' . $marker . '"}';
+				}
+				mysqli_close($dbcon);	
+				
+		}
+		
+	}
+	else
+	{
+		echo "Goody try";
+	}
 }
 
 ?>
