94b313e4f1e8b4f5c049635c396e66d99e30272d
Tyler Drury <that_canadianguy@hotmail.com>
2015-02-23 12:05:44 -0500

    page updates
    updated/refomated admin and members page

--- Users/admin-page.php
+++ Users/admin-page.php
@@ -1,8 +1,9 @@
 <?php
+//require '../include/html.php';
 session_start();
 //require 'secure.php';
 //secureLogin();
-if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1))
+if(!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1))
 {
    header("Location: login.php");
    exit();
@@ -13,42 +14,46 @@ if (!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 1))
 <head>
 <title>Admin page</title>
 <meta charset=utf-8>
-<link rel="stylesheet" type="text/css" href="includes.css">
-<style type="text/css">
+<link rel='stylesheet' type='text/css' href='includes.css'>
+<style type='text/css'>
 #midcol { width:88%; }
 #midcol p { margin-left:-4%; }
 </style>
 </head>
 <body>
-<div id="container">
-<?php include("includes/header-admin.php"); ?>
-<?php include("includes/nav.php"); ?>
-<?php include("includes/info-col.php"); ?>
-	<div id="content"><!-- Start of the member's page content. -->
+<div id='container'>
 <?php
-echo '<h2>Welcome to the Admin Page ';
-if (isset($_SESSION['fname']))
+require 'includes/header-admin.php';
+require 'includes/nav.php';
+require 'includes/info-col.php';
+?>
+	<div id='content'><!-- Start of the member's page content. -->
+        <h2>Welcome to the Admin Page 
+<?php
+if(isset($_SESSION['fname']))
 {
 	//echo "{$_SESSION['fname']}";
 }
-if (isset($_SESSION['uname']))
+if(isset($_SESSION['uname']))
 {
 	echo "{$_SESSION['uname']}";
 	//$sname = $_SESSION['uname'];
 	//$sname = $_SESSION['fname'];
 }
-echo '</h2>';
 ?>
-<div id="midcol">
-	<h3>You have permission to:</h3><p>&#9632;Edit and delete a record.</p>
-	<p>&#9632;Use the View members button to page through all the members.</p>
-	<p>&#9632;Use the Search button to locate a particular member</p>
-	<p>&#9632;Use the Users button to locate a member's Username </p>
-	<p>&nbsp;</p></div>
-<!-- End of the members page content. -->
-</div></div>	
-	<div id="footer">
-		<?php include("includes/footer.php"); ?>
-	</div>
+        </h2>
+        <div id='midcol'>
+            <h3>You have permission to:</h3><p>&#9632;Edit and delete a record.</p>
+            <p>&#9632;Use the View members button to page through all the members.</p>
+            <p>&#9632;Use the Search button to locate a particular member</p>
+            <p>&#9632;Use the Users button to locate a member's Username </p>
+            <p>&nbsp;</p>
+        </div>
+            <!-- End of the members page content. -->
+    </div>
+</div>	
+<div id='footer'>
+<?php require 'includes/footer.php';?>
+</div>
 </body>
 </html>
--- Users/members-page.php
+++ Users/members-page.php
@@ -1,4 +1,5 @@
 <?php
+//require ../include/html.php';
 session_start();
 require 'includes/secure.php';
 //secureLogin();
@@ -11,59 +12,53 @@ if(!isset($_SESSION['user_level']) or ($_SESSION['user_level'] != 0))
 <!doctype html>
 <html lang=en>
 <head>
-<title>Members' page</title>
-<meta charset=utf-8>
-<link rel="stylesheet" type="text/css" href="includes.css">
-<script type="text/javascript" src="//code.jquery.com/jquery-2.1.0.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<!-- Include JS File Here -->
-
-<style type="text/css">
+    <title>Members' page</title>
+    <meta charset=utf-8>
+    <link rel='stylesheet' type='text/css' href='includes.css'>
+    <script type='text/javascript' src='//code.jquery.com/jquery-2.1.0.min.js'></script>
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+    <!-- Include JS File Here -->
+    <style type='text/css'>
 #mid-right-col { text-align:center; margin:auto;margin-right: 25%; margin-top: -10%
 }
 #midcol h3 { font-size:130%; margin-top:0; margin-bottom:0; 
 }
-</style>
+    </style>
 </head>
 <body>
-<div id="container">
-
-<?php include("includes/nav.php"); ?>
-<?php include("includes/info-col.php"); ?>
-<?php include("my_parse_file.php"); ?>
-
-
-	<div id="content"><!-- Start of the member's page content. -->
+<div id='container'>
 <?php
-echo '<h2>Welcome to the Members\' Page ';
-if (isset($_SESSION['fname']))
+require 'includes/nav.php';
+require 'includes/info-col.php';
+require 'my_parse_file.php';
+?>
+	<div id='content'><!-- Start of the member's page content. -->
+        <h2>Welcome to the Members' Page 
+<?php
+if(isset($_SESSION['fname']))
 {
 	//echo "{$_SESSION['fname']}";
 	//$sname = $_SESSION['uname'];
 	//$sname = $_SESSION['fname'];
 }
-if (isset($_SESSION['uname']))
+if(isset($_SESSION['uname']))
 {
 	echo "{$_SESSION['uname']}";
 	//$sname = $_SESSION['uname'];
 	//$sname = $_SESSION['fname'];
 }
-
-echo '</h2>';
 ?>
+        </h2>
 
-
-<div id="midcol">
+        <div id='midcol'>
 <?php
-
 //Query the database
-//Query the database
-require ('config.php');
+require 'config.php';
 $q = "SELECT * FROM users WHERE uname = '$_SESSION[uname]'";		
 $result = mysqli_query ($dbcon, $q);
 
 //Count the returned rows
-if( mysqli_num_rows($result) != 0)
+if(mysqli_num_rows($result) != 0)
 {
 	//Turn the results in to an array
 	while($rows = $result->fetch_assoc())
@@ -79,36 +74,35 @@ if( mysqli_num_rows($result) != 0)
 }
 else
 {
-	echo "No Results";
-	// If there was a problem.
-		echo '<p class="error">Please try again.</p>';
+	echo 'No Results';
+	//If there was a problem.
+    echo "<p class='error'>Please try again.</p>";
 	//exit();
 }
 mysqli_close($dbcon);
 ?>
-
-<div id="mid-left-col">
-
+            <div id='mid-left-col'>
 </body>
-<h3>Member's Events</h3>
-<p>Welcome to the members area.
-<br>Browse the many portals here: Play as a guest or log in and save your progress.<br>
-Enter one of our events to win prizes.<br>Get a hold of our Merchandise today! </p>
-</div>
-<div id="mid-right-col">
-<h3>Special offers to Members only.</h3>
-<div id="nav">
-<ul>
-<li><a href="..\index.php" title="Play Game">Play Game</a></li>
-</ul>
-</div>
-<br>
-<p><b>T-Shirts &pound;100.00</b></p>
-<img alt="Polo shirt" title="Polo shirt" height="207" src="images/polo.png" width="280"><br>
-<br>
-
-</div>
-</div></div><!-- End of the Members' page content. -->
-</div>	
-<?php include('includes/footer.php'); ?></body>
+                <h3>Member's Events</h3>
+                <p>Welcome to the members area.<br>
+                <br>Browse the many portals here: Play as a guest or log in and save your progress.<br>
+                Enter one of our events to win prizes.<br>Get a hold of our Merchandise today!</p>
+            </div><!--end mid-left-col-->
+            <div id='mid-right-col'>
+                <h3>Special offers to Members only.</h3>
+                <div id='nav'>
+                    <ul>
+                    <li><a href='..\index.php' title='Play Game'>Play Game</a></li>
+                    </ul>
+                </div>
+                <br>
+                <p><b>T-Shirts &pound;100.00</b></p>
+                <img alt='Polo shirt' title='Polo shirt' height='207' src='images/polo.png' width='280'><br>
+                <br>
+            </div><!--end  mid-rid-col-->
+        </div><!--end mid-col-->
+    </div><!-- End of the Members' page content. -->
+</div><!--end container-->
+<?php require 'includes/footer.php';?>
+</body>
 </html>
