5db31da768be0be726fff82a6a5c4b8a6f54dee3
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-02-24 15:38:27 -0500

    my_parse_file db connection updates

--- my_parse_file.php
+++ my_parse_file.php
@@ -1,39 +1,39 @@
 <?php 
-require ('Users/config.php');
+require ('config.php');
 
-session_start();
 
-if (isset($_SESSION['uname']))
+if( isset( $_POST) )
 {
-	echo "{$_SESSION['uname']}";
-	if( isset( $_POST) )
+	if (!empty($_POST['fname'])  && !empty($_POST['money']) && !empty($_POST['tokens']) &&!empty($_POST['prestige']) && !empty($_POST['m_marker']))
 	{
-		if ( !empty($_POST['money']) || !empty($_POST['tokens']) || !empty($_POST['prestige']) || !empty($_POST['m_marker']))
+		$fname = $_POST["fname"];
+		$money = $_POST["money"];
+		$tokens = $_POST["tokens"];
+		$prestige = $_POST["prestige"];
+		$markers = $_POST["m_marker"];
+		//$q = 'UPDATE users SET fname="Learning JAVA" WHERE user_id=1';
+		$q = "UPDATE users SET fname='$fname', money='$money', tokens='$tokens' ,prestige='$prestige', m_marker='$markers' WHERE user_id=user_id";
+		
+		$result = mysqli_query ($dbcon, $q);
+		if(!$result )
 		{
-				$money = $_POST["money"];
-				$tokens = $_POST["tokens"];
-				$prestige = $_POST["prestige"];
-				$marker = $_POST["m_marker"];
-				$q = "UPDATE users SET money='$money', tokens='$tokens' ,prestige='$prestige', m_marker='$marker' WHERE   uname = '$_SESSION[uname]'";
-				echo "Awesome";
-				$result = mysqli_query ($dbcon, $q);
-				if(!$result )
-				{
-				  die('Could not update data: ' . mysql_error());
-				}
-				else
-				{
-				  echo '{"money":"' . $money . '", "tokens":"' . $tokens . '","prestige":"' . $prestige . ',"m_marker":"' . $marker . '"}';
-				}
-				mysqli_close($dbcon);	
-				
+		  die('Could not update data: ' . mysql_error());
 		}
+		else
+		{
+		  echo '{"fname":"' . $fname . '", "lname":"' . $money . '"}';
+	
+		}
+		mysqli_close($dbcon);	
 		
 	}
-	else
-	{
-		echo "Goody try";
-	}
+
+}
+else
+{
+	$fname = $_POST[" "];
+	//$lname = $_POST[" "];
+	echo "Goody try";
 }
 
 ?>
