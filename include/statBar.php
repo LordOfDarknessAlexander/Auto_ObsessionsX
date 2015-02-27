5ed8e4e022a65685cfd484a3891afabcc74be46f
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-02-27 02:42:23 -0500

    .

--- include/statBar.php
+++ include/statBar.php
@@ -1,9 +1,18 @@
-<div id='statBar'>    
-	<label id='fname'>User: </label>         
+<div id='statBar'>             
 	<label id='money'>Money: </label>
 	<label id='tokens'>Tokens:</label>
 	<label id='prestige'>Prestige:</label>
 	<label id='m_marker'>Mile Markers:</label>  
-	
+	<?php
+
+session_start();
+echo '<h2>Welcome to Auto-Obsessions\' Game ';
+if (isset($_SESSION['uname']))
+{
+	echo "{$_SESSION['uname']}";
+}
+echo '</h2>';
+
+?>
 <div id="status"></div>
 </div> 
