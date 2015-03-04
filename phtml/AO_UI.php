731aa8329ad89097b498ca6d6b595fab9b2f692e
Tyler Drury <that_canadianguy@hotmail.com>
2015-03-04 18:56:25 -0500

    .

--- phtml/AO_UI.php
+++ phtml/AO_UI.php
@@ -2,15 +2,20 @@
 //this file contains functions for generating
 //HTML UI elements
 function backBtn(){?>
-    <button id='backBtn'>Back</button>
+    <button id='backBtn'></button>
 <?php
 }
 function homeBtn(){?>
-    <button id='homeBtn'>Home</button>
+    <button id='homeBtn'></button>
 <?php
 }
 function carInfoLabel(){?>
     <label id='carInfo'>Default Info</label>
+<?php
+}
+function pb($id){
+    //outputs a html progress bar, defaulted to 0?>
+    <progress id='<?php echo $id;?>' value='0.0'></progress>
 <?php
 }
 ?>
--- phtml/repair.php
+++ phtml/repair.php
@@ -1,10 +1,128 @@
 <?php
-//$ROOT_DIR = $_SERVER["DOCUMENT_ROOT"]."/phtml/";	//root path on server
+require_once 'AO_UI.php';
+
+//function rb($str){<button id='rbTransmission' class='rb'></button>}
+//function ub($str){<button id='ubEngine' class='ub'></button>}
 ?>
 <div id='RepairShop'>
     <h1>Repair Shop</h1>
     <p id='error'></p>
-    <button id='backBtn'>Back</button>
+<?php
+    backBtn();
+?>
+	<button id='addFunds'>Add Funds</button>
+    <!--a id='addFunds' href='<php echo rootURL() . 'state/funds.php';?>'>Add Funds</a-->
+    <h2 id='dt'>Drivetrain</h2>	
+    <div id='drivetrain'>
+Engine:
+<button id='ubEngine' class='ub'></button>
+<button id='rbEngine' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbEngine' value='0.0'></progress>
+<br>Transmission:
+<button id='ubTransmission' class='ub'></button>
+<button id='rbTransmission' class='rb'></button>
+<br><?php //br(); pb('pbTransmission'); br();?>
+<progress id='pbTransmission' value='0.0'></progress>
+<br>Drive Axel:
+<button id='ubAxel' class='ub'></button>
+<button id='rbAxel' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbAxel' value='0.0'></progress>
+<br>Exhaust:
+<button id='ubExhaust' class='ub'></button>
+<button id='rbExhaust' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbExhaust' value='0.0'></progress>
+<br>
+        <!--Fuel System:
+        <button id='ubFuel' class='ub'></button>
+        <button id='rbFuel' class='rb'></button>
+        <br>
+        <progress id='pbFuel' value='0.0'></progress>
+        <br-->
+    </div>
+    <h2 id='body'>Body</h2>
+    <div id='body'>
+Chasis:
+<button id='ubChasis' class='ub'></button>
+<button id='rbChasis' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbChasis' value='0.0'></progress>
+<br>Body Panels:
+<button id='ubPanels' class='ub'></button>
+<button id='rbPanels' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbPanels' value='0.0'></progress>
+<br>Paint:
+<button id='ubPaint' class='ub'></button>
+<button id='rbPaint' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbPaint' value='0.0'></progress>
+<br>Chrome:
+<button id='ubPH0' class='ub'></button>
+<button id='rbPH0' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbPH0' value='0.0'></progress>
+<br>
+        <!--PLACEHOLDER:
+        <button id='ubBtn4' class='ub'></button>
+        <button id='rb4' class='rb'></button>
+        <br>
+        <progress id='pb4' value='0.0'></progress>
+        <br-->
+    </div>
+    <h2 id='interior'>Interior</h2>
+    <div id='interior'>
+Seats:
+<button id='ubSeats' class='ub'></button>
+<button id='rbSeats' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbSeats' value='0.0'></progress>
+<br>Carpet:
+<button id='ubCarpet' class='ub'></button>
+<button id='rbCarpet' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbCarpet' value='0.0'></progress>
+<br>Dash:
+<button id='ubDash' class='ub'></button>
+<button id='rbDash' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbDash' value='0.0'></progress>
+<br>Door Panels:
+<button id='ubPanels' class='ub'></button>
+<button id='rbPanels' class='rb'></button>
+<br><?php //pb('pb'); br();?>
+<progress id='pbPanels' value='0.0'></progress>
+<br>
+        <!--PLACEHOLDER:
+        <button id='ubBtn4' class='ub'></button>
+        <button id='rb' class='rb'></button>
+        <br>
+        <progress id='pb4' value='0.0'></progress>
+        <br-->
+    </div>
+    <!---->
+    <h2 id='docs'>Documentation</h2>
+    <div id='docs'>
+Ownership:<button id='ubOwnership' class='ub'></button><br>
+<progress id='pbOwnership' value='0.0'></progress>
+<br>Build Sheet:
+<button id='ubBuildSheet' class='ub'></button><br>
+<?php //pb('pb'); br();?>
+<progress id='pbBuildSheet' value='0.0'></progress>
+<br>History:
+<button id='ubHistory' class='ub'></button><br>
+<?php //pb('pb'); br();?>
+<progress id='pbHistory' value='0.0'></progress>
+<br>Restoration:<button id='ubPH0' class='ub'></button><br>
+<?php //pb('pb'); br();?>
+<progress id='pbPH0' value='0.0'></progress>
+<br>
+        <!--PLACEHOLDER:<button id='ubBtn4' class='ub'></button><br>
+        <progress id='pb4' value='0.0'></progress>
+        <br-->
+    </div>
     <!--img id='userCar'-->
     <!--div id='upgrades'>
         <h2>Upgrades</h2>
@@ -25,114 +143,4 @@
         <button id='fixBtn4'></button><br>
         <button id='fixBtn5'></button>
     </div-->	
-	<button id='addFunds'>Add Funds</button>
-    <!--a id='addFunds' href='<php echo rootURL() . 'state/funds.php';?>'>Add Funds</a-->
-    <h2 id='dt'>Drivetrain</h2>	
-    <div id='drivetrain'>
-Engine:
-<button id='ubEngine' class='ub'></button>
-<button id='rbEngine' class='rb'></button>
-<br>
-<progress id='pbEngine' value='0.0'></progress>
-<br>Transmission:
-<button id='ubTransmission' class='ub'></button>
-<button id='rbTransmission' class='rb'></button>
-<br>
-<progress id='pbTransmission' value='0.0'></progress>
-<br>Drive Axel:
-<button id='ubAxel' class='ub'></button>
-<button id='rbAxel' class='rb'></button>
-<br>
-<progress id='pbAxel' value='0.0'></progress>
-<br>Exhaust:
-<button id='ubExhaust' class='ub'></button>
-<button id='rbExhaust' class='rb'></button>
-<br>
-<progress id='pbExhaust' value='0.0'></progress>
-<br>
-        <!--Fuel System:
-        <button id='ubFuel' class='ub'></button>
-        <button id='rbFuel' class='rb'></button>
-        <br>
-        <progress id='pbFuel' value='0.0'></progress>
-        <br-->
-    </div>
-    <h2 id='body'>Body</h2>
-    <div id='body'>
-Chasis:
-<button id='ubChasis' class='ub'></button>
-<button id='rbChasis' class='rb'></button>
-<br>
-<progress id='pbChasis' value='0.0'></progress>
-<br>Body Panels:
-<button id='ubPanels' class='ub'></button>
-<button id='rbPanels' class='rb'></button>
-<br>
-<progress id='pbPanels' value='0.0'></progress>
-<br>Paint:
-<button id='ubPaint' class='ub'></button>
-<button id='rbPaint' class='rb'></button>
-<br>
-<progress id='pbPaint' value='0.0'></progress>
-<br>Chrome:
-<button id='ubPH0' class='ub'></button>
-<button id='rbPH0' class='rb'></button>
-<br>
-<progress id='pbPH0' value='0.0'></progress>
-<br>
-        <!--PLACEHOLDER:
-        <button id='ubBtn4' class='ub'></button>
-        <button id='rb4' class='rb'></button>
-        <br>
-        <progress id='pb4' value='0.0'></progress>
-        <br-->
-    </div>
-    <h2 id='interior'>Interior</h2>
-    <div id='interior'>
-Seats:
-<button id='ubSeats' class='ub'></button>
-<button id='rbSeats' class='rb'></button>
-<br>
-<progress id='pbSeats' value='0.0'></progress>
-<br>Carpet:
-<button id='ubCarpet' class='ub'></button>
-<button id='rbCarpet' class='rb'></button>
-<br>
-<progress id='pbCarpet' value='0.0'></progress>
-<br>Dash:
-<button id='ubDash' class='ub'></button>
-<button id='rbDash' class='rb'></button>
-<br>
-<progress id='pbDash' value='0.0'></progress>
-<br>Door Panels:
-<button id='ubPanels' class='ub'></button>
-<button id='rbPanels' class='rb'></button>
-<br>
-<progress id='pbPanels' value='0.0'></progress>
-<br>
-        <!--PLACEHOLDER:
-        <button id='ubBtn4' class='ub'></button>
-        <button id='rb' class='rb'></button>
-        <br>
-        <progress id='pb4' value='0.0'></progress>
-        <br-->
-    </div>
-    <!---->
-    <h2 id='docs'>Documentation</h2>
-    <div id='docs'>
-Ownership:<button id='ubOwnership' class='ub'></button><br>
-<progress id='pbOwnership' value='0.0'></progress>
-<br>Build Sheet:
-<button id='ubBuildSheet' class='ub'></button><br>
-<progress id='pbBuildSheet' value='0.0'></progress>
-<br>History:
-<button id='ubHistory' class='ub'></button><br>
-<progress id='pbHistory' value='0.0'></progress>
-<br>Restoration:<button id='ubPH0' class='ub'></button><br>
-<progress id='pbPH0' value='0.0'></progress>
-<br>
-        <!--PLACEHOLDER:<button id='ubBtn4' class='ub'></button><br>
-        <progress id='pb4' value='0.0'></progress>
-        <br-->
-    </div>
 </div>
