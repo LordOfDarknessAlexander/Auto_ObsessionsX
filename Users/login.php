d64e11185dca65d634a9b31ace998cacf1a5a3cb
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-03-10 15:21:43 -0400

    login.inc file

--- Users/login.php
+++ Users/login.php
@@ -65,16 +65,12 @@ exit(); // Cancel the rest of the script
 	} // End of submit conditional
 ?>
 <!-- Display the form fields-->
-<div id='loginfields'>
-    <h2>Login</h2>
-    <form action='login.php' method='post'>
-        <label class='label' for='email'>Email Address:</label><br>
-        <input id='email' type='text' name='email' size='30' maxlength='60' value='<?php if (isset($_POST['email'])) echo $_POST['email'];?>'><br>
-        <label class='label' for='psword'>Password:</label><br>
-        <input id='psword' type='password' name='psword' size='12' maxlength='12' value='<?php if (isset($_POST['psword'])) echo $_POST['psword']; ?>'>&nbsp;Between 8 and 12 characters<br>
-        <input id='submit' type='submit' name='submit' value='Login'>
-    </form><br>
+<!-- Display the form fields-->
+<div id="loginfields">
+<?php include ('includes/login_page.inc'); ?>
 </div>
+<p>&nbsp;</p>
+<p class="cntr"><a href="forgot.php"><b>Forgotten your password?</b></a></p>
 <br>
 </div><!--end content-->
 <?php require '../phtml/legal.php';?>
