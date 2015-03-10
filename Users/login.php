c0a580c401b2720e5d4c27b511f444357cc257a6
Tyler Drury <that_canadianguy@hotmail.com>
2015-03-10 19:03:23 -0400

    .

--- Users/login.php
+++ Users/login.php
@@ -43,21 +43,20 @@ require_once '../include/dbConnect.php';
 		$result = mysqli_query ($AO_DB->con, $q); 
 		// Check the result
 		if (@mysqli_num_rows($result) == 1) {//The user input matched the database record
-		// Start the session, fetch the record and insert the three values in an array
-		session_start();
-		$_SESSION = mysqli_fetch_array ($result, MYSQLI_ASSOC);
-$_SESSION['user_level'] = (int) $_SESSION['user_level']; // Ensure the user level is an integer
-// The login page redirects the user either to the admin page or the users search page
-// Use a ternary operation to set the URL
-$url = ($_SESSION['user_level'] === 51) ? 'admin_page.php' : 'members-page.php';
-header('Location: ' . $url); // The user is directed to the appropriate page
-exit(); // Cancel the rest of the script
-	mysqli_free_result($result);
-//	mysqli_close($dbcon);
-	} else { // If no match was found
-	
-	echo '<p class="error">The email address and password entered do not match our records.<br>Perhaps you need to register, click the Register button on the header menu</p>';
-	}
+            // Start the session, fetch the record and insert the three values in an array
+            session_start();
+            $_SESSION = mysqli_fetch_array ($result, MYSQLI_ASSOC);
+            $_SESSION['user_level'] = (int) $_SESSION['user_level']; // Ensure the user level is an integer
+            // The login page redirects the user either to the admin page or the users search page
+            // Use a ternary operation to set the URL
+            $url = ($_SESSION['user_level'] === 51) ? 'admin_page.php' : 'members-page.php';
+            header('Location: ' . $url); // The user is directed to the appropriate page
+            mysqli_free_result($result);
+            exit(); // Cancel the rest of the script
+        //	mysqli_close($dbcon);
+        } else { // If no match was found
+            echo '<p class="error">The email address and password entered do not match our records.<br>Perhaps you need to register, click the Register button on the header menu</p>';
+        }
 	} else { // If there was a problem
 		echo '<p class="error">Please try again.</p>';
 	}
@@ -67,7 +66,14 @@ exit(); // Cancel the rest of the script
 <!-- Display the form fields-->
 <!-- Display the form fields-->
 <div id="loginfields">
-<?php include ('includes/login_page.inc'); ?>
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <p><label class="label" for="email">Email Address:</label>
+        <input id="email" type="text" name="email" size="30" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" > </p>
+            <p><label class="label" for="psword">Password:</label>
+        <input id="psword" type="password" name="psword" size="12" maxlength="12" value="<?php if (isset($_POST['psword'])) echo $_POST['psword']; ?>" ></p>
+        <br><br><p><input id="submit" type="submit" name="submit" value="Login"></p>
+    </form>
 </div>
 <p>&nbsp;</p>
 <p class="cntr"><a href="forgot.php"><b>Forgotten your password?</b></a></p>
