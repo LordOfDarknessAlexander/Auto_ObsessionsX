9b62f35be9fc0b9b443232c0c3e2614939ae513c
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-03-19 18:54:48 -0400

    .

--- phtml/main.php
+++ phtml/main.php
@@ -37,26 +37,6 @@ $ROOT_URL = 'http://triosdevelopers.com/A.Sanchez/Assets/AutoObsessionsGame/';
         </div>
         <div id='main'>		
             <h1><?php echo $AO_NAME;?></h1>
-           <!---
-            <ul>
-	
-               <li><a href='javascript:void(0)' class='button play'>Start Game</a></li>
-               <li><a href='javascript:void(0)' class='button credits'>Credits</a></li>
-               
-               <li><a href='javascript:void(0)' class='button Register'>Register</a></li>
-            
-            </ul>
-		
-			<div id="loginfields">
-				<h2>Login</h2>
-				<form action="login.php" method="post">
-					<p><label class="label" for="email">Email Address:</label>
-					<input id="email" type="text" name="email" size="30" maxlength="50" value=" email" > </p>
-					<p><label class="label" for="psword">Password:</label>
-					<input id="psword" type="password" name="psword" size="12" maxlength="12" value="psword" ></p>
-					<p><input id="submit" type="submit" name="submit" value="Login"></p>
-				</form>
-			</div> ---->
 			 <?php 
 			session_start();
 
