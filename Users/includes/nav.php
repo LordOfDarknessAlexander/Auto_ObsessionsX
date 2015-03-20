2ca4555bc8075a38c03bd5461252ce872d913ed3
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-03-20 16:11:06 -0400

    .

--- Users/includes/nav.php
+++ Users/includes/nav.php
@@ -1,6 +1,5 @@
 <?php?>
 <div id='nav'><!--The side menu column contains the vertical menu-->
-    <a href='index.php' title='Play Game'>Play as Guest</a><br>
     <a href='tutorial.php' title='Tutorial'>Tutorial</a><br>
     <a href='credits.php' title='Credits'>Credits</a><br>
     <a href='profiles.php' title='Player Profile'>Profile</a><br>
