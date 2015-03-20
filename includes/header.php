900c22fb332ea319007de606aa3dca9ca0017e69
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-03-20 15:47:35 -0400

    cleanup

--- includes/header.php
+++
@@ -1,7 +0,0 @@
-<div id="header">
-<h1>Auto-Obsessions</h1>
-    <div id="reg-navigation">
-        <a href="login.php">Login</a><br>
-        <a href="safer-register-page.php">Register</a><br>
-	</div>
-</div>
