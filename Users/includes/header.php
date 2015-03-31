61fc66e94c5e316f8f0fe1049bbd4751f37e584b
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-03-31 14:59:56 -0400

    .

--- Users/includes/header.php
+++ Users/includes/header.php
@@ -1,7 +1,7 @@
 <div id="header">
 <h1>Auto-Obsessions</h1>
     <div id="reg-navigation">
-        <a href="login.php">Login</a><br>
+        <a href="../login.php">Login</a><br>
         <a href="Users/registerUser.php">Register</a><br>
 	</div>
 </div>
