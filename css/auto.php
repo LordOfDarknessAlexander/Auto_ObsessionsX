fcb71e71fc58a2e07203a5095a10668881535fd5
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-04-16 16:45:46 -0400

    new bgTile.png

--- css/auto.php
+++ css/auto.php
@@ -250,9 +250,7 @@ div#statBar{
     posAbs();
     css::size('100%', '15%');
 ?>
-    /*background-color:red;*/
 	background: url('../images/statBarTile.png');
-	/*background-size : 100%,40%;*/
 	top:0%;
 	left:0%;
 	
@@ -293,7 +291,7 @@ div#reg-navigation{
     right:0%;
     text-align:center;
     z-index:3;
-	top : 7%;
+	top : 9%;
 	
 }
 div#reg-navigation a{
@@ -305,7 +303,7 @@ div#reg-navigation a{
     font-weight: bold;
     text-decoration: none;
 	padding : 2%;
-	top : 7%;
+	top : 9%;
 }
 div#reg-navigation a:hover{
 <?php
@@ -315,7 +313,7 @@ div#reg-navigation a:hover{
 ?>  
 	color:green;
 	padding : 2%;
-	top : 7%;
+	top : 9%;
 }
 /* visited link */
 #div#main a:visited {
