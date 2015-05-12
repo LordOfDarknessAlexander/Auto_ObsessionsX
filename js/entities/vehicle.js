af2510c6bd579aae466c953335d7cf091261e75f
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-05-12 12:52:11 -0400

    small fix

--- js/entities/vehicle.js
+++ js/entities/vehicle.js
@@ -164,7 +164,7 @@ function Vehicle(Name, Make, Year, Price, carID, carInfo, parts, repairs)
 			//return Math.floor(ret * 100.0);// *  Math.random(0.0,1.25);
 				//Make condition random 
 			//this.condition = this.getCondition();
-			return this.condition +  Math.floor(Math.random(1,125) * 100) ;
+			return this.condition +  Math.floor(Math.random(1,100) * 100) ;
 			 
 		},
 		getFullName : function()
