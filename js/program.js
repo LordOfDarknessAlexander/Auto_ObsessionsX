4944e54bd899ae7e6ad17a8715be0477c9a98f67
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-05-12 00:47:35 -0400

    .

--- js/program.js
+++ js/program.js
@@ -292,7 +292,7 @@ function startGame()
 	// Fill with gradient
 	context.fillStyle = gradient;
 	//temp
-	userStats.money += 100000;
+	//userStats.money += 100000;
 	setStatBar();
 	switchStates();
 	
