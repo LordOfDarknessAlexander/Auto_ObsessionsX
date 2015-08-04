9c3ce027c6607577e99400fef060a98778b17655
Tyler Drury <that_canadianguy@hotmail.com>
2015-08-04 11:32:06 -0400

    .

--- pas/body.php
+++ pas/body.php
@@ -29,23 +29,21 @@ class aoBody{
         }
         //exit();
     }
-    public static function setBody($b){
-        // global $aoUsersDB;
+    public static function setBody($cid, $b){
+        global $aoUsersDB;
         
-        // $B = 'body';
+        $TN = getUserTableName();
+        $B = 'body';
         
-        // $res = $aoUsersDB->query(
-            // "UPDATE $tableName SET
-                // $B=$nb
-            // WHERE
-                // $CID = $carID"
-        // );
-        // if($res){
-            // $ret = intval($res->fetch_assoc()[$B]);
-            // //echo $ret;
-            // return $ret;
-        // }
-        //exit();
+        $res = $aoUsersDB->query(
+            "UPDATE $TN SET $B = $b WHERE $CID = $cid"
+        );
+        
+        if($res){
+            //echo json_encode($res);
+            return $res;
+        }
+        return null;
     }
     public static function upgradeChasis($cid, $price){
         global $aoUsersDB;
@@ -53,6 +51,7 @@ class aoBody{
         $offset = 12;   //number of bits
         $b = aoBody::getBody($cid);
         $chas = ($b & 0xF000) >> $offset;
+        //echo $chas;
         $PRO = 4;   //reassing from enum
         
         if($chas < $PRO){
@@ -64,7 +63,7 @@ class aoBody{
                 
             if($dif > 0.000008){
                 //if purchase is successful, $dif > 1
-                $tableName = getUserTableName();
+                //$tableName = getUserTableName();
                 //
                 //mask and shift values here
                 $nc = ($b == 0 ? 1 : $chas << 1);   //new chrome value
@@ -73,20 +72,24 @@ class aoBody{
                 $nb = ($b & 0x0FFF) | $shift;   //clear last bits, setting new value
                 //echo $nb;
                 //set new values
-                $res = $aoUsersDB->query(
-                    "UPDATE $tableName SET
-                        $B=$nb
-                    WHERE
-                        $CID = $carID"
-                );
+                
+                //$res = $aoUsersDB->query(
+                    //"UPDATE $tableName SET
+                        //$B=$nb
+                    //WHERE
+                        //$CID = $carID"
+                //);
                 //
-                $ret = array(
-                    'userFunds'=>$nf,
-                    'cid'=>$cid,
-                    $B=>$nb
-                );
+                if(aoBody::setBody($cid, $nb) ){
+                    $ret = array(
+                        'userFunds'=>$nf,
+                        'cid'=>$cid,
+                        $B=>$nb
+                    );
         
-                echo json_encode($ret);
+                    echo json_encode($ret);
+                    exit();
+                }
             }
             //else, new funds and user's funds are the same,
             //purchase not succussful
@@ -120,28 +123,23 @@ class aoBody{
                 $nb = ($b & 0xF0FF) | $shift;   //clear last bits, setting new value
                 //echo $nb;
                 //set new values
-                $res = $aoUsersDB->query(
-                    "UPDATE $tableName SET
-                        $B=$nb
-                    WHERE
-                        $CID = $carID"
-                );
-                //
-                $ret = array(
-                    'userFunds'=>$nf,
-                    'cid'=>$cid,
-                    $B=>$nb
-                );
+               if(aoBody::setBody($cid, $nb) ){
+                    $ret = array(
+                        'userFunds'=>$nf,
+                        'cid'=>$cid,
+                        $B=>$nb
+                    );
         
-                echo json_encode($ret);
+                    echo json_encode($ret);
+                    exit();
+                }
             }
             echo 'could not purchase upgrade, insufficent funds';
         }
         echo 'could not upgrade part, already fully upgraded';
     }
     public static function upgradePaint($cid, $price){
-       global $aoUsersDB;
-        
+        //
         $offset = 4;   //number of bits
         $b = aoBody::getBody($cid);
         $paint = ($b & 0x00F0) >> $offset;
@@ -156,7 +154,6 @@ class aoBody{
                 
             if($dif > 0.000008){
                 //if purchase is successful, $dif > 1
-                $tableName = getUserTableName();
                 //
                 //mask and shift values here
                 $np = ($paint == 0 ? 1 : $paint << 1);   //new chrome value
@@ -165,28 +162,24 @@ class aoBody{
                 $nb = ($b & 0xFF0F) | $shift;   //clear last bits, setting new value
                 //echo $nb;
                 //set new values
-                $res = $aoUsersDB->query(
-                    "UPDATE $tableName SET
-                        $B=$nb
-                    WHERE
-                        $CID = $carID"
-                );
-                //
-                $ret = array(
-                    'userFunds'=>$nf,
-                    'cid'=>$cid,
-                    $B=>$nb
-                );
+                if(aoBody::setBody($cid, $nb) ){
+                    $ret = array(
+                        'userFunds'=>$nf,
+                        'cid'=>$cid,
+                        $B=>$nb
+                    );
         
-                echo json_encode($ret);
+                    echo json_encode($ret);
+                    exit();
+                }
             }
             echo 'could not purchase upgrade, insufficent funds';
         }
         echo 'could not upgrade part, already fully upgraded';
+        exit();
     }
     public static function upgradeChrome($cid, $price){
-        global $aoUsersDB;
-        
+        //
         $b = aoBody::getBody($cid);
         $chrome = ($b & 0x000F) >> 0;
         $PRO = 4;   //reassing from enum
@@ -200,7 +193,6 @@ class aoBody{
                 
             if($dif > 0.000008){
                 //if purchase is successful, $dif > 1
-                $tableName = getUserTableName();
                 //
                 //mask and shift values here
                 $nc = ($chrome == 0 ? 1 : $chrome << 1);   //new chrome value
@@ -208,20 +200,16 @@ class aoBody{
                 $nb = ($b & 0xFFF0) | $nc;   //clear last bits, setting new value
                 //echo $nb;
                 //set new values
-                $res = $aoUsersDB->query(
-                    "UPDATE $tableName SET
-                        $B=$nb
-                    WHERE
-                        $CID = $carID"
-                );
-                //
-                $ret = array(
-                    'userFunds'=>$nf,
-                    'cid'=>$cid,
-                    $B=>$nb
-                );
+                if(aoBody::setBody($cid, $nb) ){
+                    $ret = array(
+                        'userFunds'=>$nf,
+                        'cid'=>$cid,
+                        $B=>$nb
+                    );
         
-                echo json_encode($ret);
+                    echo json_encode($ret);
+                    exit();
+                }
             }
             //else, new funds and user's funds are the same,
             //purchase not succussful
