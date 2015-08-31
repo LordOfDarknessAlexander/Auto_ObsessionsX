def20fffe84ff1c5da5e21b2271f64e69a1e699b
BrendanM <danmatrix@live.com>
2015-08-31 12:25:50 -0400

    .

--- js/program.js
+++ js/program.js
@@ -1,37 +1,6 @@
 //Application main
 var AutoObessesions = {};
 var engnRv = true;
-var Allowance = {
-    CAP:( (1000 * 60) * 60) * 24,  //86400000,  //3000,
-    //INV_CAP:1.0 / CAP,    //inverse cap, multiply by this instead of dividing by CAP!
-    getDelta:function(){
-        //get diffrence between last time and now        
-        return Date.now() - Allowance.getLastTime();
-    },
-    getLastTime:function(){
-        //the last time the user collected their allowance
-        if(Storage.local !== null){
-            if('_lastAllowanceTime' in Storage.local){
-                return parseInt(Storage.local._lastAllowanceTime);
-                //userStats.money = 225000;
-            }
-        }
-        return 0;
-    },
-    setLastTime:function(){
-        if(Storage.local !== null){
-            var time = Date.now()	//a time of 0
-            Storage.local._lastAllowanceTime = JSON.stringify(time);
-        }
-    },
-    getRefreshMS:function(){
-        //return time left until button can be clicked
-        return Allowance.CAP - Allowance.getDelta();
-    },
-    getRefreshPerc:function(){
-        return Allowance.getDelta() / Allowance.CAP;
-    }
-};
 
 function garageDoor(){
 	backgroundY -= speed;
@@ -235,7 +204,7 @@ function(){
 	//Storage.local.clear();
 	//alert('doc ready!');
 	//pas.query.loadUser();   //load user stats!
-	Allowance.getLastTime();
+	//Allowance.getLastTime();
 	//setStatBar();
     load();
 	//loadUser();
